3319fd4c6372a5d92ac6c8992b049f7d1c1549df
Lokman Musliu <lokman.musliu@gmail.com>
2019-12-24 02:16:58 +0100

    Initial setup and homepage wip

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:500,700&display=swap" rel="stylesheet">
+    </head>
+    <body class="antialiased font-sans">
+        @yield('body')
+    </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'sans': 'Poppins, -apple-system, BlinkMacSystemFont',
+    },
+    colors: {
+      gray: {
+        500: '#333333',
+        400: '#4D4D4D',
+        300: '#6B6B6B',
+        200: '#808080',
+        100: '#E6E6E6',
+        50: '#F6F6F6'
+      },
+      blue: {
+        500: '#2653E3'
+      },
+      black: '#000'
+    },
+    extend: {}
+  },
+  variants: {},
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    require('tailwind-bootstrap-grid')({
+      gridGutterWidth: '32px',
+    })
+  ]
+}
