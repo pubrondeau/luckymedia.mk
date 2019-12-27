ca93e550d342e760406403b3ccc6eb6f5d707f57
Lokman Musliu <lokman.musliu@gmail.com>
2019-12-27 01:23:14 +0100

    Featured work section finished

--- source/index.blade.php
+++ source/index.blade.php
@@ -89,18 +89,42 @@
       </div>
     </div>
 
+
+    {{-- What we do --}}
     <div class="container mt-32">
         <div class="row">
             <div class="col-2">
                 @include('_components.heading-sm', ['color' => 'black', 'title' => 'What we do'])
             </div>
         </div>
-        <div class="row mt-20">
+        <div class="row mt-20 mb-40">
             @foreach($page->homepage->cards as $card)
             <div class="col-4 mb-8">
                 @include('_components.card', ['icon' => $card->icon, 'title' => $card->title, 'desc' => $card->desc])
             </div>
             @endforeach
+        </div>
+    </div>
+
+    {{-- Featured Work --}}
+    <div class="bg-gray-50">
+        <div class="container pb-56">
+            <div class="row justify-center">
+              <div class="flex flex-col items-center">
+                  <div style="width: 2px;" class="h-24 bg-blue-500 -mt-12"></div>
+                  <svg class="block fill-current w-12 h-12 text-black mt-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48">
+                      <path d="M0 24l10.286-10.286L20.57 24 10.286 34.286 0 24zM13.714 10.286L24 0l10.286 10.286L24 20.57 13.714 10.286zM13.714 37.714L24 27.43l10.286 10.285L24 48 13.714 37.714zM27.429 24l10.285-10.286L48 24 37.714 34.286 27.43 24z"/>
+                  </svg>
+                  <h5 class="text-base text-black uppercase font-medium tracking-widest mt-12">Featured Work</h5>
+              </div>
+            </div>
+            <div class="row justify-center relative mt-24">
+                <div class="col-8 dots">
+                    <div class="bg-white shadow-lg p-4">
+                        <img class="w-full h-auto" src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
