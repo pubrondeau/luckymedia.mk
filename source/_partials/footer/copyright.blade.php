b938902e4fd5e2ef96d6dd01a74e851a0ddab9be
pubrondeau <pubrondeau@gmail.com>
2021-02-06 02:24:52 +0100

    Update copyright.blade.php

--- source/_partials/footer/copyright.blade.php
+++ source/_partials/footer/copyright.blade.php
@@ -4,12 +4,12 @@
         <div class="row flex flex-col">
             <div class="col-12">
                 <p class="font-medium text-lg text-black mb-32">
-                    Borce Jovanoski nr.32,<br>
-                    Gostivar 1230, <br>
-                    North Macedonia
+                    Saint Avé<br>
+                    Morbihan, <br>
+                    France
                 </p>
                 <div class="flex flex-col md:flex-row md:justify-between mb-12">
-                    <p class="text-sm md:text-lg font-medium text-black order-2 md:order-none">© 2020 Lucky Media — All Rights Reserved</p>
+                    <p class="text-sm md:text-lg font-medium text-black order-2 md:order-none">© 2021 STATIK — Tous droits réservés</p>
                     <div class="flex flex-row mb-5 md:mb-0">
                         <a href="https://www.facebook.com/LuckyMediaInc/" class="text-lg text-black mr-10">Facebook</a>
                         <a href="https://www.instagram.com/luckymedia.dev/" class="text-lg text-black">Instagram</a>
