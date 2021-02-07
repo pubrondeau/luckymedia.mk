d415e2cc1e777d90629302cc27aee124c6d7def9
pubrondeau <pubrondeau@gmail.com>
2021-02-07 17:10:36 +0100

    Update project.blade.php

--- source/_partials/footer/project.blade.php
+++ source/_partials/footer/project.blade.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="row relative">
             <div class="md:col-7 flex flex-col">
-                <h1 class="text-3xl md:text-5xl text-white font-bold">Let’s work together</h1>
-                <p class="font-medium text-base text-white mt-3">We're passionate about providing innovative, bright ideas and execute them into a wonderful experience.</p>
+                <h1 class="text-3xl md:text-5xl text-white font-bold">Travaillons ensemble</h1>
+                <p class="font-medium text-base text-white mt-3">Nous sommes passionnés par la création d'idées innovantes et brillantes et les mettons en œuvre pour en faire une expérience merveilleuse.</p>
                 <a href="/contact"
                    class="w-40 py-3 border border-white text-center text-white uppercase mt-10 hover:bg-white hover:text-black animate">Contact
                     us</a>
